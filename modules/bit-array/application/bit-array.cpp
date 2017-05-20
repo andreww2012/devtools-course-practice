aa9d9dc7042155360e6660239170e20d3ff6fd76
Andrew Kazakov <tosk-nn@yandex.ru>
2017-05-20 22:34:31 +0300

    BitArray: added simple application

---
+++ modules/bit-array/application/bit-array.cpp
@@ -0,0 +1,15 @@
+// Copyright 2017 Kazakov Andrew
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+#include "include/bit_array.h"
+
+int main(int argc, const char** argv) {
+  // Application app;
+  // std::string output = app(argc, argv);
+  // printf("%s\n", output.c_str());
+  printf("Hello world!");
+  return 0;
+}
